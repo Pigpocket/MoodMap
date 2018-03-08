7bebe3b96ee3ecb4b2a63d3a6e0a6e0024c9ac71
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-03-07 18:14:16 -0800

    Add ResultsViewController

---
+++ MoodMap/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.swift
+//  MoodMap
+//
+//  Created by Tomas Sidenfaden on 3/7/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RatingCell") as! TableViewCell
+        
+        
+        return cell
+    }
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+}
