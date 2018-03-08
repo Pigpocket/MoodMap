a373807198ee1875ba0f698d30e74650bdacf19f
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-03-07 16:43:35 -0800

    Add category labels

--- MoodMap/QuestionsVC.swift
+++ MoodMap/QuestionsVC.swift
@@ -13,8 +13,8 @@ class QuestionsVC: UIViewController {
     
     // MARK: Properties
     
-    
-    let questions: [String] = ["I feel like the team is communicating effectively. I know what is expected of me and what to expect of others. When problems arise we work together to find solutions", "This project is being managed effectively. The project manager or team lead understands how to manage team members, resources, processes and deadlines", "Our team is able to achieve deadlines and complete tasks as expected", "We have the skills, experience and resources to complete project tasks on schedule", "Blah blah blah we are able to lorum ipsum dolor and lorem ipsum dolor and etc etc etc for the love of all that is good and well"]
+    let categories: [String] = ["Communication", "Management", "Schedule", "Resources", "Productivity"]
+    let questions: [String] = ["I feel like the team is communicating effectively. I know what is expected of me and what to expect of others. When problems arise we work together to find solutions", "This project is being managed effectively. The project manager or team lead understands how to manage team members, resources, processes and deadlines", "Our team is able to achieve deadlines and complete tasks as expected", "We have the resources and people necessary to complete this project", "I am able to be productive. My time is used efficiently and effectively"]
     
     var selectedIndex = 0
     
@@ -23,6 +23,7 @@ class QuestionsVC: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -38,6 +39,7 @@ class QuestionsVC: UIViewController {
         }
         
         questionLabel.text = questions[selectedIndex]
+        categoryLabel.text = categories[selectedIndex]
         
     }
     
@@ -54,6 +56,7 @@ class QuestionsVC: UIViewController {
         }
 
         questionLabel.text = questions[selectedIndex]
+        categoryLabel.text = categories[selectedIndex]
     }
     
 
